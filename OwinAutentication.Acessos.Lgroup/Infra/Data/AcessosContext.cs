58492b990dd965e5d237e729e0db360704dfa8c5
JeanLLopes <jeanllopes@outlook.com>
2016-11-29 22:03:39 -0200

    Add project files.

---
+++ OwinAutentication.Acessos.Lgroup/Infra/Data/AcessosContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using OwinAutentication.Acessos.Lgroup.DomainModel;
+
+namespace OwinAutentication.Acessos.Lgroup.Infra.Data
+{
+    public class AcessosContext
+        :IdentityDbContext<Usuario>
+    {
+        //Estamos setando a string de conexão do webConfig
+        //com o name AAIdentity1
+        public AcessosContext() 
+            : base("AAIdentity1")
+        {        }
+    }
+}
